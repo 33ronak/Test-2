c89af521568098c1517e5dcd70a31a20e0d6ca5b
33ronak <08arnab98@gmail.com>
2024-04-06 07:55:38 +0530

    third commit

--- test/index.js
+++ test/index.js
@@ -1,6 +1,8 @@
-console.log("Git Test");
+console.log("DOM Test");
 
-// getElementById Method
+/*-----------------------------------------------------------------------*/
+
+//  getElementById Method:
 const mainHeading = document.getElementById('main-heading');
 mainHeading.textContent = 'Fruit World';
 mainHeading.style.color = 'orange';
@@ -14,3 +16,24 @@ basket.style.color = 'green';
 
 const footer = document.getElementById('thanks');
 footer.innerHTML = '<p>Please visit us again</p>';
+
+/*-----------------------------------------------------------------------*/
+
+//  getElementsByClassName Method:
+const fruit = document.getElementsByClassName('fruit');
+fruit[2].style.backgroundColor = 'yellow';
+
+for (let i = 0; i < fruit.length; i++) {
+  fruit[i].style.fontWeight = 'bold';
+}
+
+/*-----------------------------------------------------------------------*/
+
+//  getElementsByTagName Method:
+
+const listItems = document.getElementsByTagName('li');
+listItems[4].style.color = 'blue';
+
+for (let i = 0; i < listItems.length; i++) {
+  listItems[i].style.fontStyle = 'italic';
+}
