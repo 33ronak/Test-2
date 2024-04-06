6b1d88b5123dc0c2471d80b689137fba6c93dc9d
33ronak <08arnab98@gmail.com>
2024-04-06 07:42:46 +0530

    second commit

--- test/index.js
+++ test/index.js
@@ -1,1 +1,16 @@
 console.log("Git Test");
+
+// getElementById Method
+const mainHeading = document.getElementById('main-heading');
+mainHeading.textContent = 'Fruit World';
+mainHeading.style.color = 'orange';
+
+const header = document.getElementById('header');
+header.style.backgroundColor = 'green';
+header.style.borderBottom = 'solid orange';
+
+const basket = document.getElementById('basket-heading');
+basket.style.color = 'green';
+
+const footer = document.getElementById('thanks');
+footer.innerHTML = '<p>Please visit us again</p>';
